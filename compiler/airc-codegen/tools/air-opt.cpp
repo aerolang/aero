22f17f8552f486f52d696bc8dfa59469207affa1
Bradley Bridges <bradley@bbridges.io>
2025-04-09 10:53:23 -0700

    tmp: llvm now also

--- compiler/airc-codegen/tools/air-opt.cpp
+++ compiler/airc-codegen/tools/air-opt.cpp
@@ -1,11 +1,11 @@
 #include "mlir/include/mlir/InitAllDialects.h"
-#include "mlir/include/mlir/Pass/PassManager.h"
-#include "mlir/include/mlir/Pass/PassRegistry.h"
+#include "mlir/include/mlir/InitAllPasses.h"
 #include "mlir/include/mlir/Tools/mlir-opt/MlirOptMain.h"
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   mlir::registerAllDialects(registry);
+  mlir::registerAllPasses();
 
   return mlir::asMainReturnCode(
     mlir::MlirOptMain(argc, argv, "AIR Pass Driver", registry));
