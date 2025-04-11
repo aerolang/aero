8750e60626177e91e27133623638ae41ab99431f
Bradley Bridges <bradley@bbridges.io>
2025-04-10 17:18:51 -0700

    tmp: start airc-syntax

---
+++ compiler/airc-syntax/src/parser.rs
@@ -0,0 +1,9 @@
+mod air {
+    // The pest_derive macro makes "Rule" public, so placing this into a nested
+    // module to prevent it from being exposed.
+    use pest_derive::Parser;
+
+    #[derive(Parser)]
+    #[grammar = "air.pest"]
+    pub struct AirParser;
+}
